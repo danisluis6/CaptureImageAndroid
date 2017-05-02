154ee5345a51629e1e99b597bae31e77ca8d82a8
danisluis7 <vuongluis@gmail.com>
2017-05-03 06:56:56 +0700

    Lorence

---
+++ app/src/main/java/vuongluis/myapplication/MainActivity.java
@@ -0,0 +1,142 @@
+package vuongluis.myapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView result;
+    private Bitmap photo;
+
+    private final int CAMERA_REQUEST = 2;
+    private final int MY_PERMISSIONS_REQUEST_CODE = 1;
+
+    /** Check permission for Android with version >= 6.0 **/
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != MY_PERMISSIONS_REQUEST_CODE) {
+            return;
+        }
+        boolean isGranted = true;
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                isGranted = false;
+                break;
+            }
+        }
+
+        if (isGranted) {
+            startApplication();
+        }else{
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void setPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CODE);
+    }
+
+    public void startApplication() {
+        initComponents();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (checkPermissions()) {
+            startApplication();
+        } else {
+            setPermissions();
+        }
+    }
+
+    private void initComponents() {
+        button = (Button)this.findViewById(R.id.button);
+        result = (ImageView)this.findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            previewCapturedImage(data);
+
+            // Call this method to get the uri from the bitmap
+            Uri tempUri = getImageUri(getApplicationContext(), photo);
+
+            // Call the method to get the actual path
+            File finalFile = new File(getRealPathFromURI(tempUri));
+
+            String encodedImage = encodeImage(photo);
+            Log.d("TAG",encodedImage);
+        }
+
+    }
+
+    private void previewCapturedImage(Intent data) {
+        photo = (Bitmap) data.getExtras().get("data");
+        result.setImageBitmap(photo);
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+    }
+
+    private String encodeImage(Bitmap bm)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encImage;
+    }
+}
